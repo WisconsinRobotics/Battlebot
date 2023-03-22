6be4bad542870cd4dd341ec57f3a4aa54330a5ab
ramarlow <98787850+ramarlow@users.noreply.github.com>
2023-03-22 15:50:06 -0500

    added espbluetooth arduino program and gamepadbluetooth.py
    gamepadbluetooth.py takes gamepad inputs and sends them over bluetooth serial, espbluetooth interprets them and makes the robot move (technically functional, but sometimes the motors get stuck on)

---
+++ espbluetooth/espbluetooth.ino
@@ -0,0 +1,73 @@
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
+#include "BluetoothSerial.h"
+
+//#define USE_PIN // Uncomment this to use PIN during pairing. The pin is specified on the line below
+const char *pin = "1234"; // Change this to more secure PIN.
+
+String device_name = "ESP32-BT-Slave";
+uint8_t buf[2];
+
+const uint8_t PIN_MOTOR_1A = 2;
+const uint8_t PIN_MOTOR_1B = 4;
+
+const uint8_t PIN_MOTOR_2A = 18;
+const uint8_t PIN_MOTOR_2B = 19;
+
+uint8_t input_1 = 128;
+uint8_t input_2 = 128;
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin(device_name); //Bluetooth device name
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+  //Serial.printf("The device with name \"%s\" and MAC address %s is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str(), SerialBT.getMacString()); // Use this after the MAC method is implemented
+  #ifdef USE_PIN
+    SerialBT.setPin(pin);
+    Serial.println("Using PIN");
+  #endif
+}
+
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.print(int(buf[0]));
+    Serial.print("\t");
+    Serial.println(int(buf[1]));
+    SerialBT.readBytes(buf, 2);
+    input_1 = buf[0];
+    input_2 = buf[1];
+  }
+  if (input_1 > 127) {
+    analogWrite(PIN_MOTOR_1A, map(input_1, 128, 255, 0, 255));
+    digitalWrite(PIN_MOTOR_1B, 0);
+  } else {
+    digitalWrite(PIN_MOTOR_1A, 0);
+    analogWrite(PIN_MOTOR_1B, map(input_1, 0, 127, 255, 0));    
+  }
+
+  if (input_2 > 127) {
+    analogWrite(PIN_MOTOR_2A, map(input_2, 128, 255, 0, 255));
+    digitalWrite(PIN_MOTOR_2B, 0);
+  } else {
+    digitalWrite(PIN_MOTOR_2A, 0);
+    analogWrite(PIN_MOTOR_2B, map(input_2, 0, 127, 255, 0));    
+  }
+  
+}
---
+++ gamepadbluetooth.py
@@ -0,0 +1,38 @@
+import serial
+from struct import pack
+from time import sleep, time
+from inputs import get_gamepad
+
+
+def ser_conn(): #connect to the arduino over serial. runs once at the beginning
+    ser = serial.Serial("COM8", 115200, timeout=0.01)
+    ser.reset_input_buffer()
+    sleep(3)
+    return ser
+
+gamepad_map = { # what does each event code mean?
+    "ABS_Y":'Ljoy_y',
+    "ABS_RY":'Rjoy_y'
+}
+
+gamepad_read = { # mapping each input name to its most recent reading. values are floats from -1 to 1. dict order dictates setpoint send order
+    'Ljoy_y':128,
+    'Rjoy_y':128
+}
+
+
+def input_loop():
+    ser = ser_conn()
+    while True:
+        try:
+            gamepad = get_gamepad()
+            for event in gamepad:
+                #print(event.ev_type, event.code, event.state)
+                if event.code == "ABS_Y" or event.code == "ABS_RY":
+                    gamepad_read[gamepad_map[event.code]] = max(min(255, round((event.state / 32768 + 1) * 128 )), 0) # store the value to whatever input is corresponds with
+                    print("Sending " + str(list(gamepad_read.values())))
+                    ser.write(pack("cc", *[pt.to_bytes(1, 'little') for pt in gamepad_read.values()])) #send the message over serial
+        except:
+            print('no gamepad found')
+
+input_loop()
