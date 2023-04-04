3a78b4e2db32581e3ac433fa598c1d71f03e9102
ramarlow <98787850+ramarlow@users.noreply.github.com>
2023-04-03 19:16:33 -0500

    press bumper to actuate the hammer

--- gamepadbluetooth.py
+++ gamepadbluetooth.py
@@ -17,7 +17,8 @@ gamepad_map = { # what does each event code mean?
 
 gamepad_read = { # mapping each input name to its most recent reading. values are floats from -1 to 1. dict order dictates setpoint send order
     'Ljoy_y':128,
-    'Rjoy_y':128
+    'Rjoy_y':128,
+    'trigger':0
 }
 
 
@@ -28,10 +29,15 @@ def input_loop():
             gamepad = get_gamepad()
             for event in gamepad:
                 #print(event.ev_type, event.code, event.state)
-                if event.code == "ABS_Y" or event.code == "ABS_RY":
-                    gamepad_read[gamepad_map[event.code]] = max(min(255, round((event.state / 32768 + 1) * 128 )), 0) # store the value to whatever input is corresponds with
+                event_code = event.code
+                if event_code in {"BTN_TL", "BTN_TR", "ABS_Y", "ABS_RY"}:
+                    if event.code == "ABS_Y" or event.code == "ABS_RY":
+                        gamepad_read[gamepad_map[event.code]] = max(min(255, round((event.state / 32768 + 1) * 128 )), 0) # store the value to whatever input is corresponds with
+                    else:
+                        gamepad_read['trigger'] = event.state
                     print("Sending " + str(list(gamepad_read.values())))
-                    ser.write(pack("cc", *[pt.to_bytes(1, 'little') for pt in gamepad_read.values()])) #send the message over serial
+                    ser.write(pack("ccc", *[pt.to_bytes(1, 'little') for pt in gamepad_read.values()])) #send the message over serial
+                
         except:
             print('no gamepad found')
 
